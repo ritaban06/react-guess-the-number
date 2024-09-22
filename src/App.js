fbedd8652b234cb1c43d41c0c4cef5f2e03ff5a6
ritaban06 <ghoshritabancollege@gmail.com>
2024-09-22 11:51:00 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import NumberInput from './components/NumberInput';
+import ResultMessage from './components/ResultMessage';
+import Button from './components/Button';
 import './App.css';
+const App = () => {
+  const [secretNumber, setSecretNumber] = useState(Math.floor(Math.random() * 25) + 1);
+  const [message, setMessage] = useState('');
+  const [count, setCount] = useState(0);
+  const maxCount = 5;
 
-function App() {
+  useEffect(() => {
+    if (count === maxCount) {
+      setMessage('You have reached the maximum number of guesses!');
+      setHasWon(false);
+    }
+  }, [count]);
+
+  const remainingAttempts = maxCount - count;
+  const [hasWon, setHasWon] = useState(false);
+
+  const handleGuess = (value) => {
+    const guessNum = parseInt(value);
+    setCount(count + 1);
+    if (count < maxCount) {
+      if (guessNum === secretNumber) {
+        setMessage('Congratulations! You guessed it right!');
+        setHasWon(true);
+
+      }
+      else if (guessNum > secretNumber){
+        setMessage('Your guess is too high. Try again!'); 
+      }
+      else {
+        setMessage(`You Guess Too low. Try again!`);
+      }
+    }
+  };
+
+  const resetGame = () => {
+    setSecretNumber(Math.floor(Math.random() * 20) + 1);
+    setMessage('');
+    setCount(0);
+    setHasWon(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header />
+    <div className={`container ${hasWon ? 'win' : ''}`}>
+      <div className="card">
+        <div className="content">
+          <NumberInput onSubmit={handleGuess} />
+          <ResultMessage message={message} />
+          {(count === maxCount || hasWon) && (
+             <Button onClick={resetGame} text="Restart Game" />
+           )}
+        </div>
+        <div className="attempts">
+          <p>Remaining attempts: {remainingAttempts}</p>
+        </div>
+      </div>
     </div>
+    </>
   );
-}
+};
 
 export default App;
